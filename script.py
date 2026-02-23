e4e8364cb39d17b6b42f04721adeca47df67b297
Andrei Balan <andrei.balan98@gmail.com>
2026-02-23 22:11:09 +0200

    migrate to FastAPI

--- script.py
+++ script.py
@@ -1,68 +1,21 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from fastapi import FastAPI, Body
 import functions
 import json
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        match self.path:
-            case "/health":
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write(b"Hi, I'm Nanny, I'm here to help you organize!\n(list - for listing tasks)\n(add <task> - for adding a task)\n(done <task> - for setting a task as done)\n(delete <task> - to deleting a task)\n(exit - to exit program)")
-            case "/list":
-                self.send_response(200)
-                self.send_header("Content-Type", "application/json")
-                self.end_headers()
-                self.wfile.write(json.dumps(functions.loadTasks()).encode())
-            case _:
-                self.send_response(500)
-                self.end_headers()
-                self.wfile.write(b"Error: path not found")
-    def do_POST(self):
-        match self.path:
-            case "/add":
-                length = int(self.headers["Content-Length"])
-                task = self.rfile.read(length).decode()
-                
-                functions.addTask(task)
+app = FastAPI()
 
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write(b"Succesfully added task!")
-            case _:
-                self.send_response(500)
-                self.end_headers()
-                self.wfile.write(b"Error: path not found")
-    def do_PATCH(self):
-        match self.path:
-            case "/done":
-                length = int(self.headers["Content-Length"])
-                task = self.rfile.read(length).decode()
-                
-                functions.doneTask(task)
+@app.get("/health")
+def healthCheck():
+    return "Hi, I'm Nanny, I'm here to help you organize!\n(list - for listing tasks)\n(add <task> - for adding a task)\n(done <task> - for setting a task as done)\n(delete <task> - to deleting a task)\n(exit - to exit program)"
+              
+@app.post("/add")
+def addTask(task: str = Body()):
+    functions.addTask(task)
 
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write(b"Succesfully doned task!")
-            case _:
-                self.send_response(500)
-                self.end_headers()
-                self.wfile.write(b"Error: path not found")
-    def do_DELETE(self):
-        match self.path:
-            case "/delete":
-                length = int(self.headers["Content-Length"])
-                task = self.rfile.read(length).decode()
-                
-                functions.deleteTask(task)
+@app.patch("/done")
+def doneTask(task: str = Body()):
+    functions.doneTask(task)
 
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write(b"Succesfully deleted task!")
-            case _:
-                self.send_response(500)
-                self.end_headers()
-                self.wfile.write(b"Error: path not found")
-
-server = HTTPServer(("localhost", 6969), Handler)
-server.serve_forever()
+@app.delete("/delete")
+def deleteTask(task: str = Body()):
+    functions.deleteTask(task)
