4078d07d1f2c2821cc1e20f25ab74240e7d9564b
Andrei Balan <andrei.balan98@gmail.com>
2026-02-23 19:25:28 +0200

    /health, /list

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import functions
+import json
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -7,10 +9,15 @@ class Handler(BaseHTTPRequestHandler):
                 self.send_response(200)
                 self.end_headers()
                 self.wfile.write(b"Hi, I'm Nanny, I'm here to help you organize!\n(list - for listing tasks)\n(add <task> - for adding a task)\n(done <task> - for setting a task as done)\n(delete <task> - to deleting a task)\n(exit - to exit program)")
+            case "/list":
+                self.send_response(200)
+                self.send_header("Content-Type", "application/json")
+                self.end_headers()
+                self.wfile.write(json.dump(functions.loadTasks()).encode())
             case _:
                 self.send_response(500)
                 self.end_headers()
                 self.wfile.write(b"Error: path not found")
 
-server = HTTPServer(("localhost", 8080), Handler)
+server = HTTPServer(("localhost", 6969), Handler)
 server.serve_forever()
